0b35cefc950732a60c244561679864cef8203bbd
Yoki <thekingmaker0012@gmail.com>
2024-05-23 11:36:05 +0530

    first commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <>
+      <div>
+        <div className="text-2xl font-bold text-indigo-600">Hello World!</div>
+        <div>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam totam
+          explicabo quis suscipit natus ab, quasi
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Profile;
